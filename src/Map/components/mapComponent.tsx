960e15653e64d24896a610aeebf9116901dc8296
5aad <saad_anwer@live.com>
2024-09-24 05:54:39 +0200

    change layers name to indicate which one is working in prod and dev

--- src/Map/components/mapComponent.tsx
+++ src/Map/components/mapComponent.tsx
@@ -36,32 +36,32 @@ const MapComponent: FC<MapComponentProps> = ({
     >
       {/* layers toggle */}
       <LayersControl position="topright">
-        <BaseLayer checked name="OpenStreetMap Default">
+        <BaseLayer checked name="OpenStreetMap (dev, prod)">
           <TileLayer
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
         </BaseLayer>
 
-        <BaseLayer name="OpenStreetMap Black & White ">
+        <BaseLayer name="OpenTopoMap (dev, prod)">
+          <TileLayer
+            attribution='&copy; <a href="https://opentopomap.org">OpenTopoMap</a> 
+      (CC-BY-SA)'
+            url="https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png"
+          />
+        </BaseLayer>
+
+        <BaseLayer name="OpenStreetMap Black & White (dev) ">
           <TileLayer
             attribution='&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
           />
         </BaseLayer>
 
-        <BaseLayer name="Satellite">
+        <BaseLayer name="Satellite (dev)">
           <TileLayer
             attribution='&copy; CNES, Distribution Airbus DS, © Airbus DS, © PlanetObserver (Contains Copernicus Data) | &copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://tiles.stadiamaps.com/tiles/alidade_satellite/{z}/{x}/{y}{r}.jpg"
-          />
-        </BaseLayer>
-        
-        <BaseLayer name="OpenTopoMap">
-          <TileLayer
-            attribution='&copy; <a href="https://opentopomap.org">OpenTopoMap</a> 
-      (CC-BY-SA)'
-            url="https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png"
           />
         </BaseLayer>
       </LayersControl>
