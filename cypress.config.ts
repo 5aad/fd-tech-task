e98b56a372b3ef7f95e8a38a56c20779476a1a09
5aad <saad_anwer@live.com>
2025-03-04 21:30:29 +0100

    init cypress e2e

---
+++ cypress.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+ e2e: {
+    baseUrl: "http://localhost:5173", // Default Vite dev server
+    supportFile: false
+  },
+  component: {
+    devServer: {
+      framework: "react",
+      bundler: "vite",
+    },
+  },
+});
---
+++ cypress/e2e/map.cy.ts
@@ -0,0 +1,5 @@
+describe("Home Page", () => {
+    it("should load the homepage", () => {
+      cy.visit("/");
+    });
+  });
